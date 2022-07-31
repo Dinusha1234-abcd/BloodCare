6d16138594b9d4ea9f99e8d4f3b079e8c299d4e7
Malsha Sudusinghe <62473043+SSMalsha@users.noreply.github.com>
2022-07-31 21:57:26 +0530

    Update page.admin.dashboard.js

--- web_client/src/pages/page.admin.dashboard.js
+++ web_client/src/pages/page.admin.dashboard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SlideMenuAdmin from "../components/component.slidemenu.admin";
+import '../assests/css/admin.dashboard.css';
 export default function AdminDashboard() {
     return(
         <SlideMenuAdmin headerName={"Dashboard"}/>
