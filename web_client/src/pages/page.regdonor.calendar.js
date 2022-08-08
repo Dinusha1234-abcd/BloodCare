6c149b21b53ffce6eebbd8bf31f79dd9697126cc
Lakeesha98 <2019cs075@stu.ucsc.lk>
2022-08-08 12:40:00 +0530

    add regDonor home page

--- web_client/src/pages/page.regdonor.calendar.js
+++ web_client/src/pages/page.regdonor.calendar.js
@@ -3,6 +3,11 @@ import SlideMenuRegDonor from "../components/component.slidemenu.registerDonor";
 
 export default function RegDonorCalender() {
   return(
+    <div>
       <SlideMenuRegDonor headerName={"Calender"}/>
+      <div class="calendar-rectangle">
+      <div id="calendar-content" class="calendar-content"></div>
+    </div>
+    </div>
   );
 }
