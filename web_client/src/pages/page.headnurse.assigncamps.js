1af93e6864001d3d715db40ffb68f0ede3039e7f
hansika123 <hansikak1998@gmail.com>
2022-08-13 01:07:14 +0530

    headnurse

--- web_client/src/pages/page.headnurse.assigncamps.js
+++ web_client/src/pages/page.headnurse.assigncamps.js
@@ -1,13 +1,11 @@
-/*import React, { useState, useEffect } from "react";
-import { Bar } from 'react-chartjs-2';
-import Calendar from 'react-calendar';
+import React, { useState, useEffect } from "react";
 import SlideMenuHeadNurse from "../components/headnurse/component.slidemenu.headNurse";
-import Camp from "../components/headnurse/component.camp.display.headnurse";
 import pastCamp from '../assests/images/pastCamp.png';
 import OngoingCamp from '../assests/images/ongoingCamp.png';
 import futureCamp from '../assests/images/furtureCamp.png';
 import PendingCamp from '../assests/images/camp.png';
 import '../assests/css/headnurse/headnurse.assigncamps.css';
+import Camp from "../components/headnurse/component.camp.display.headnurse";
 
 
 
@@ -22,15 +20,13 @@ export default function HeadNurseAssignCamps() {
         <div>
           <SlideMenuHeadNurse headerName={"Blood Camps"} passData={passData} />
           <div id={`${slidemenu ? 'state-change-true' : 'state-change-false'}`} >
-            <div id="row-1">
-              <div id="upcomming">
-                <h3>Upcomming Blood Camps</h3>
-                 
+            <div id="view-count">
+              <div id="upcomming" > 
+                <h3>Upcomming Blood Camps</h3>   
               </div>
             
-              <div id="past">
+              <div id="past"  >
                 <h3>Past Blood Camps</h3>
-                <div id="camp-count" display='inline'> <Camp image={pastCamp} name="Completed Camps"  />  <Camp image={futureCamp} name="Upcomming Camps" status={true} /> </div>
               </div>
             </div>
             <div id="row-2">
@@ -40,4 +36,4 @@ export default function HeadNurseAssignCamps() {
     
         </div>
       );
-    }   */
+    }   
