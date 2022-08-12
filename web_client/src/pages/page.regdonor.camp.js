150d050ea805c107bcca769312a6f1f99f510228
Lakeesha98 <2019cs075@stu.ucsc.lk>
2022-08-12 12:38:36 +0530

    add regdonor some css changes

--- web_client/src/pages/page.regdonor.camp.js
+++ web_client/src/pages/page.regdonor.camp.js
@@ -6,7 +6,17 @@ export default function RegDonorCamp() {
         <div>
             <SlideMenuRegDonor headerName={"Upcoming Camp Details"}/>
             <div id="camp-container">
-           
+            </div>
+            <div id="camp-container2">
+            </div>
+            <div id="camp-container3">
+            </div>
+            <div id='d-history-records-table-pageButton'>
+                <a className='page-navigation'>{"<< "}  </a> 
+                <a className='page-navigation'>{" Prev"}  </a> 
+                <a className='page-navigation'>1</a>
+                <a className='page-navigation'>{"Next "}</a> 
+                <a className='page-navigation'>{">>"}</a> 
             </div>
         </div>
     );
