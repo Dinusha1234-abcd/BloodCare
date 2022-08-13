26329694a2021cee3a2001fbddf083b5f0994984
Lakeesha98 <2019cs075@stu.ucsc.lk>
2022-08-13 18:00:00 +0530

    add regdonor blood camp page

--- web_client/src/pages/page.regdonor.camp.js
+++ web_client/src/pages/page.regdonor.camp.js
@@ -3,6 +3,9 @@ import SlideMenuRegDonor from "../components/component.slidemenu.registerDonor";
 import '../assests/css/page.regdonor.camp.css';
 import futureCamp from '../assests/images/furtureCamp.png';
 import OngoingCamp from '../assests/images/ongoingCamp.png';
+import pastCamp from '../assests/images/pastCamp.png';
+import PendingCamp from '../assests/images/camp.png';
+import map from '../assests/images/map.PNG';
 export default function RegDonorCamp() {
     return (
         <div>
@@ -11,35 +14,114 @@ export default function RegDonorCamp() {
             <div id="camp-container1"> 
                 <div id="camp-details">
                     <img id="campimg" src={futureCamp} />
-                    <h3>YOU CAN HELP SAVE A LIFE!</h3>
+                    <h3 id="camp-name">YOU CAN HELP SAVE A LIFE!</h3>
                     <br></br>
-                    <table>
+                    <table id="camp-details-table">
                         <tr>
-                            <th>Date : </th>
-                            <th>August 13, 2022</th>
-                            <th><i class="fa-solid fa-calendar-day"></i></th>
+                            <td>Date </td>
+                            <td>: August 13, 2022</td>
+                            <td><i class="fa-solid fa-calendar-day"></i></td>
                         </tr>
                         <tr>
-                            <th>Location : </th>
-                            <th>Panadura</th>
+                            <td>Location  </td>
+                            <td>: Panadura</td>
                         </tr>
                         <tr>
-                            <th>Place :</th>
-                            <th>MOH Hall</th>
+                            <td>Place :</td>
+                            <td>: MOH Hall</td>
                         </tr>
                         <tr>
-                            <th>Time :</th>
-                            <th>8.00 a.m. - 3.00 p.m.</th>
+                            <td>Time :</td>
+                            <td>: 8.00 a.m. - 3.00 p.m.</td>
                         </tr>
                     </table>
-                    
+                    <button id="register">Register Now</button>
+                    <img id="mapimg" src={map} />
                 </div>     
             </div>
             <div id="camp-container2">
+            <div id="camp-details">
+                    <img id="campimg" src={OngoingCamp} />
+                    <h3 id="camp-name">Blood Donation Drive</h3>
+                    <br></br>
+                    <table id="camp-details-table">
+                        <tr>
+                            <td>Date </td>
+                            <td>: August 18, 2022</td>
+                            <td><i class="fa-solid fa-calendar-day"></i></td>
+                        </tr>
+                        <tr>
+                            <td>Location  </td>
+                            <td>: Kalutara</td>
+                        </tr>
+                        <tr>
+                            <td>Place :</td>
+                            <td>: MOH Hall</td>
+                        </tr>
+                        <tr>
+                            <td>Time :</td>
+                            <td>: 8.00 a.m. - 3.00 p.m.</td>
+                        </tr>
+                    </table>
+                    <button id="register">Register Now</button>
+                    <img id="mapimg" src={map} />
+                </div>     
             </div>
             <div id="camp-container3">
+            <div id="camp-details">
+                    <img id="campimg2" src={pastCamp} />
+                    <h3 id="camp-name">BE A HERO DONTAE BLOOD</h3>
+                    <br></br>
+                    <table id="camp-details-table">
+                        <tr>
+                            <td>Date </td>
+                            <td>: August 29, 2022</td>
+                            <td><i class="fa-solid fa-calendar-day"></i></td>
+                        </tr>
+                        <tr>
+                            <td>Location  </td>
+                            <td>: Rathmalana</td>
+                        </tr>
+                        <tr>
+                            <td>Place :</td>
+                            <td>: YMCA</td>
+                        </tr>
+                        <tr>
+                            <td>Time :</td>
+                            <td>: 8.00 a.m. - 3.00 p.m.</td>
+                        </tr>
+                    </table>
+                    <button id="register">Register Now</button>
+                    <img id="mapimg" src={map} />
+                </div>     
             </div>
             <div id="camp-container4">
+            <div id="camp-details">
+                    <img id="campimg" src={PendingCamp} />
+                    <h3 id="camp-name">Give Blood and Save a Life</h3>
+                    <br></br>
+                    <table id="camp-details-table">
+                        <tr>
+                            <td>Date </td>
+                            <td>: September 13, 2022</td>
+                            <td><i class="fa-solid fa-calendar-day"></i></td>
+                        </tr>
+                        <tr>
+                            <td>Location  </td>
+                            <td>: Horana</td>
+                        </tr>
+                        <tr>
+                            <td>Place :</td>
+                            <td>: MOH Hall</td>
+                        </tr>
+                        <tr>
+                            <td>Time :</td>
+                            <td>: 8.00 a.m. - 3.00 p.m.</td>
+                        </tr>
+                    </table>
+                    <button id="register">Register Now</button>
+                    <img id="mapimg" src={map} />
+                </div>     
             </div>
             <div id='d-history-records-table-pageButton'>
                 <a className='page-navigation'>{"<< "}  </a> 
