f8afa10f0f51432c6a8b0ae2fe98a0fcdebb3592
Lakeesha98 <2019cs075@stu.ucsc.lk>
2022-08-13 07:46:29 +0530

    add regdonor some changes

--- web_client/src/pages/page.regdonor.camp.js
+++ web_client/src/pages/page.regdonor.camp.js
@@ -1,12 +1,30 @@
 import React from "react";
 import SlideMenuRegDonor from "../components/component.slidemenu.registerDonor";
 import '../assests/css/page.regdonor.camp.css';
+import futureCamp from '../assests/images/furtureCamp.png';
+import OngoingCamp from '../assests/images/ongoingCamp.png';
 export default function RegDonorCamp() {
     return (
         <div>
             <SlideMenuRegDonor headerName={"Upcoming Camp Details"}/>
             <input type="text" id='input-donate-date' placeholder=" &#xf002;"/> 
-            <div id="camp-container1">      
+            <div id="camp-container1"> 
+                <div id="camp-details">
+                    <img id="campimg" src={futureCamp} />
+                    <h3>YOU CAN HELP SAVE A LIFE!</h3>
+                    <br></br>
+                    <table id="camp-details-table"></table>
+                    <tr>
+                        <th>Date :</th>
+                        <th>13-08-2022</th>
+                        <th><i class="fa-solid fa-calendar-day"></i></th>
+                    </tr>
+                    <tr>
+                        <th>
+                            
+                        </th>
+                    </tr>
+                </div>     
             </div>
             <div id="camp-container2">
             </div>
