6c7adfd4c4d9fd4c993db194eef533fdde504838
Dinusha1234-abcd <62270984+Dinusha1234-abcd@users.noreply.github.com>
2022-08-14 19:19:13 +0530

    Update add component files and css files

---
+++ web_client/src/components/component.headNurse.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import '../assests/css/component.headNurse.css';
+export default function HeadNurse() {
+
+    return (
+        <div>
+            <div id='headNurse-contanier'> 
+            <h3>Head Nurse Details</h3>
+            <input type="text" id='input-headNurse' placeholder=" &#xf002; Enter NIC Number"/> 
+            <button id='add-headNurse'>+ Add New</button>
+            <br/><br/>
+            <table id="headNurse-table">
+                <tr>
+                    <th id='headNurse-number'>Id</th>
+                    <th>NIC Number</th>
+                    <th>Name</th>
+                    <th>Mobile Number</th>
+                    <th>Email</th>
+                    <th id='headNurse-action'>Action</th>
+                    
+                </tr>
+                <tr>
+                    <td>110</td>
+                    <td>88234567V</td>
+                    <td>Samanthi Perera</td>
+                    <td>0777123456</td>
+                    <td>samnthikaperera@gmail.com</td>
+                    <td>
+                        <button id='view-button-headNurse-view'>View</button>
+                        <button id='view-button-headNurse-remove'>Remove</button>
+                    </td>
+                   
+                </tr>
+                <tr>
+                    <td>111</td>
+                    <td>901234567V</td>
+                    <td>Sewandi Wathsala</td>
+                    <td>0761234567</td>
+                    <td>sewandiwathsala@gmail.com</td>
+                    <td>
+                        <button id='view-button-headNurse-view'>View</button>
+                        <button id='view-button-headNurse-remove'>Remove</button>
+                    </td>
+          </tr>
+                <tr>
+                    <td>112</td>
+                    <td>871235567V</td>
+                    <td>Gimhani Senanayake</td>
+                    <td>0782345678</td>
+                    <td>gimhanisenanayake@gmail.com</td>
+                    <td>
+                        <button id='view-button-headNurse-view'>View</button>
+                        <button id='view-button-headNurse-remove'>Remove</button>
+                    </td>
+              </tr>
+                <tr>
+                    <td>113</td>
+                    <td>8512345678V</td>
+                    <td>Kamala Silva</td>
+                    <td>0782347778</td>
+                    <td>kamalasilva@gmail.com</td>
+                    <td>
+                        <button id='view-button-headNurse-view'>View</button>
+                        <button id='view-button-headNurse-remove'>Remove</button>
+                    </td>
+                     </tr>
+                <tr>
+                    <td>114</td>
+                    <td>891334567V</td>
+                    <td>Sathya Suriya</td>
+                    <td>0782347678</td>
+                    <td>sathyasurya@gmail.com</td>
+                    <td>
+                        <button id='view-button-headNurse-view'>View</button>
+                        <button id='view-button-headNurse-remove'>Remove</button>
+                    </td>
+                </tr>
+             </table>
+            <div id='headNurse-pageButton'>
+                <a className='page-navigation'>{"<< Prev"}  </a> 
+                <a className='page-navigation'>1</a>
+                <a className='page-navigation'>{"Next >>"}</a> 
+            </div>
+         </div>
+        </div>
+    )
+}
---
+++ web_client/src/pages/page.clusterAdmin.donors.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import SlideMenuClusterAdmin from "../components/component.slidemenu.clusterAdmin";
+import '../assests/css/page.clusterAdmin.donors.css'
+export default function ClusterAdminDonors() {
+    const [slidemenu, setSlideMenu] = useState(true);
+    const passData = (data) => {
+        setSlideMenu(data);
+    };
+
+
+    return (
+        <div>
+            <SlideMenuClusterAdmin headerName={"Donors"} passData={passData} />
+            <div id={`${slidemenu ? 'state-change-true' : 'state-change-false'}`} >
+            <div id='donors-contanier'> 
+            <h3>Donors Details</h3>
+            <input type="text" id='input-donors' placeholder=" &#xf002; Enter NIC Number"/> 
+            <button id='add-donors'>+ Add New</button>
+            <br/><br/>
+            <table id="donors-table">
+                <tr>
+                    <th id='donors-number'>Id</th>
+                    <th>NIC Number</th>
+                    <th>Name</th>
+                    <th>Blood Type</th>
+                    <th>Last Donation Date</th>
+                    <th id='donors-action'>Action</th>
+
+                </tr>
+                <tr>
+                    <td>101</td>
+                    <td>891234567V</td>
+                    <td>Namal Perera</td>
+                    <td id="blood-type"><b>O+</b> </td>
+                    <td>2018/02/04</td>
+                    <td>
+                        <button id='view-button-donors-view'>View</button>
+                        <button id='view-button-donors-remove'>Remove</button>
+                    </td>
+
+                </tr>
+                <tr>
+                    <td>102</td>
+                    <td>901234567V</td>
+                    <td>Sewandi Ranasingh</td>
+                    <td id="blood-type"><b>AB+</b> </td>
+                    <td>2019/09/04</td>
+                   <td>
+                        <button id='view-button-donors-view'>View</button>
+                        <button id='view-button-donors-remove'>Remove</button>
+                    </td>
+          </tr>
+                <tr>
+                    <td>103</td>
+                    <td>871234567V</td>
+                    <td>Sathya Ranasingh</td>
+                    <td id="blood-type"><b>A+</b> </td>
+                    <td>2019/09/04</td>
+                       <td>
+                        <button id='view-button-donors-view'>View</button>
+                        <button id='view-button-donors-remove'>Remove</button>
+                    </td>
+              </tr>
+                <tr>
+                    <td>104</td>
+                    <td>891224567V</td>
+                    <td>Kamal Silva</td>
+                    <td id="blood-type"><b>B+</b> </td>
+                    <td>2019/09/04</td>
+                    <td>
+                        <button id='view-button-donors-view'>View</button>
+                        <button id='view-button-donors-remove'>Remove</button>
+                    </td>
+                     </tr>
+                <tr>
+                    <td>105</td>
+                    <td>891334567V</td>
+                    <td>Sathya Suriya</td>
+                    <td id="blood-type"><b>AB+</b> </td>
+                    <td>2019/09/04</td>
+                     <td>
+                        <button id='view-button-donors-view'>View</button>
+                        <button id='view-button-donors-remove'>Remove</button>
+                    </td>
+                </tr>
+             </table>
+            <div id='donors-pageButton'>
+                <a className='page-navigation'>{"<< Prev"}  </a> 
+                <a className='page-navigation'>1</a>
+                <a className='page-navigation'>{"Next >>"}</a> 
+            </div>
+         </div>
+        </div>
+        </div>
+    )
+
+} 
