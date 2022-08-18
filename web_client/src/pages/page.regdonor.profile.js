997b00952bed7235334d337670529649a73e3cd5
Lakeesha98 <2019cs075@stu.ucsc.lk>
2022-08-19 04:45:28 +0530

    update

--- web_client/src/pages/page.regdonor.profile.js
+++ web_client/src/pages/page.regdonor.profile.js
@@ -1,9 +1,19 @@
 import React from "react";
 import SlideMenuRegDonor from "../components/component.slidemenu.registerDonor";
+import '../assests/css/page.regdonor.profile.css'
 export default function RegDonorProfile() {
     return(
         <div>
             <SlideMenuRegDonor headerName={"Profile"}/>
+            <div id="profile-form">
+            <h3 id="profile-page-donor-name">Viraj Kariyawasam</h3>
+            <h4 id="profile-page-actor">Donor</h4>
+            <form id="regDonor-profile-form">
+
+                <button id="donor-save">Save</button>
+                <button id="donor-cancel">Cancel </button>
+            </form>
+            </div>
         </div>
     );
 }
--- web_client/src/pages/page.regdonor.registerform.js
+++ web_client/src/pages/page.regdonor.registerform.js
@@ -9,19 +9,19 @@ export default function RegisterForm() {
                 <h3 id="register-form-name">Register Form</h3>
                 <h4 id="register-form-camp-name">YOU CAN HELP SAVE A LIFE!</h4>
                 <form id="reg-form">
-                    <label for="camp-name">Camp Name</label> 
-                    <label for="camp-id">Camp ID</label><br></br>
-                    <input type="text" id="camp-name" name="camp-name" value="YOU CAN HELPSAVE A LIFE!"></input>
-                    <input type="text" id="camp-id" name="camp-id" value="0012"></input>
-                    <label for="donor-name">Name</label>
-                    <label for="donor-NIC">NIC</label><br></br>
-                    <input type="text" id="donor-name" name="donor-name" value="Mr. Viraj Kariyawasam"></input>
-                    <input type="text" id="donor-NIC" name="donor-NIC" value="20005671338v"></input><br></br>
+                    <label id="regDonor-camp-name" for="camp-name">Camp Name</label> 
+                    <label id="camp-id" for="camp-id">Camp ID</label><br></br>
+                    <input type="text" id="regDonor-camp-name" name="camp-name" value="YOU CAN HELPSAVE A LIFE!"></input>
+                    <input type="text" id="regDonor-camp-id" name="camp-id" value="0012"></input><br></br><br></br>
+                    <label id="donor-name" for="donor-name">Name</label>
+                    <label id="donor-NIC" for="donor-NIC">NIC</label><br></br>
+                    <input type="text" id="regDonor-name" name="donor-name" value="Mr. Viraj Kariyawasam"></input>
+                    <input type="text" id="regDonor-NIC" name="donor-NIC" value="20005671338v"></input><br></br><br></br>
                     <label for="vegiterian">Are you vegetarian or not?</label><br></br>
-                    <input type="radio" id="vegetarian" name="vegitarian" value="vegitarian"></input>
-                    <label for="vegetarian">Vegetarian</label>
-                    <input type="radio" id="non" name="non" value="non"></input>
-                    <label for="non">Non-Vegetarian</label><br></br>
+                    <input type="radio" id="regDonor-vegetarian" name="vegitarian" value="vegitarian"></input>
+                    <label  for="vegetarian">Vegetarian</label>
+                    <input type="radio" id="regDonor-non" name="non" value="non"></input>
+                    <label for="non">Non-Vegetarian</label><br></br><br></br>
                     <button id="donor-register">Register</button>
                     <button id="donor-cancel">Cancel </button>
                 </form>
