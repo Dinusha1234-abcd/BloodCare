4e9323f743e17a384eeeb33f447aaf7fb5403ce3
Dinusha1234-abcd <62270984+Dinusha1234-abcd@users.noreply.github.com>
2022-09-08 19:57:02 +0530

    add

--- web_client/src/components/component.driver.js
+++ web_client/src/components/component.driver.js
@@ -1,5 +1,5 @@
-import React,{useState, useEffect}   from 'react';
-import  axios  from 'axios';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import '../assests/css/component.drivers.css';
 export default function Drivers() {
     const [formReg, setFormReg] = useState(false);
@@ -17,14 +17,14 @@ export default function Drivers() {
     const [searchData, setSearchData] = useState("");
 
 
-    useEffect((() => {getDriverData() }), [])
+    useEffect((() => { getDriverData() }), [])
     function getDriverData() {
 
         const clusterAdminNic = localStorage.getItem('userNic');
         const clusterAdmin = {
             clusterAdminNic
         }
-         
+
         axios.post("http://localhost:8070/medicalstaff/selectdriver", clusterAdmin).then(
             (res) => {
 
@@ -37,8 +37,8 @@ export default function Drivers() {
             })
 
     }
-    
-     
+
+
     function addDriver(e) {
         e.preventDefault()
         const clusterAdminNic = localStorage.getItem('userNic');
@@ -49,7 +49,7 @@ export default function Drivers() {
             NIC,
             address,
             email,
-            mobileNumber 
+            mobileNumber
         }
         if (firstName.length == 0) {
             setMessage("Please Enter First Name ");
@@ -87,7 +87,7 @@ export default function Drivers() {
 
                     } else {
                         setMessage("Network Connection issue");
-                        
+
                     }
 
                 }
@@ -97,25 +97,25 @@ export default function Drivers() {
             })
         }
     }
-    
+
     const list = [];
-    if(searchData==""){ 
-    for (let i = 0; i < data.length; i++) {
-        list.push(
-        <> <tr>
-            <td>{data[i]['staffId']}</td>
-            <td>{data[i]['userNic']}</td>
-            <td> {data[i]['firstName'] + " " + data[i]['lastName']}</td>
-            <td>{data[i]['phoneNumber']}</td>
-            <td>{data[i]['email']}</td>
-            <td>
-                <button id='view-button-doctor-view' onClick={show}>View</button>
-                <button id='view-button-doctor-remove'>Remove</button>
-            </td> 
-            </tr>
-        </>)
-    }
-    } else{
+    if (searchData == "") {
+        for (let i = 0; i < data.length; i++) {
+            list.push(
+                <> <tr>
+                    <td>{data[i]['staffId']}</td>
+                    <td>{data[i]['userNic']}</td>
+                    <td> {data[i]['firstName'] + " " + data[i]['lastName']}</td>
+                    <td>{data[i]['phoneNumber']}</td>
+                    <td>{data[i]['email']}</td>
+                    <td>
+                        <button id='view-button-doctor-view' onClick={show}>View</button>
+                        <button id='view-button-doctor-remove'>Remove</button>
+                    </td>
+                </tr>
+                </>)
+        }
+    } else {
         for (let i = 0; i < 10; i++) {
             if (searchData == data[i]['userNic']) {
                 list.push(
@@ -126,7 +126,7 @@ export default function Drivers() {
                         <td>{data[i]['phoneNumber']}</td>
                         <td>{data[i]['email']}</td>
                         <td>
-                        <button id='view-button-doctor-view'  >View</button>   
+                            <button id='view-button-doctor-view'  >View</button>
                             <button id='view-button-doctor-remove'>Remove</button>
                         </td>
                     </tr>
@@ -139,76 +139,76 @@ export default function Drivers() {
 
     return (
         <div>
-            <div id='driver-contanier'> 
-            <div id={`${formReg ? 'fade-clusterAdmin' : null}`} onClick={() => { setFormReg(!formReg) }}></div>
-            <h3 id='header-clusterAdmin'>Drivers Details</h3>
-            <input type="text" id='input-driver' placeholder=" &#xf002; Enter NIC Number"/> 
-            <button id='add-driver' onClick={() => { setFormReg(!formReg) }}>+ Add New</button>
-            <br/><br/>
-            <table id="driver-table">
-                <tr>
-                    <th id='driver-number'>Id</th>
-                    <th>NIC Number</th>
-                    <th>Name</th>
-                    <th>Mobile Number</th>
-                    <th>Email</th>
-                    <th id='driver-action'>Action</th>
-                    
-                </tr>
-                {list}
-              </table>
-            <div id='driver-pageButton'>
-                <a className='page-navigation'>{"<< Prev"}  </a> 
-                <a className='page-navigation'>1</a>
-                <a className='page-navigation'>{"Next >>"}</a> 
-            </div>
-         </div>
-         <div id={`${formReg ? 'register-form-doctor-clusteradmin-active' : 'register-form-doctor-clusteradmin'}`}>
+            <div id='driver-contanier'>
+                <div id={`${formReg ? 'fade-clusterAdmin' : null}`} onClick={() => { setFormReg(!formReg) }}></div>
+                <h3 id='header-clusterAdmin'>Drivers Details</h3>
+                <input type="text" id='input-driver' placeholder=" &#xf002; Enter NIC Number" />
+                <button id='add-driver' onClick={() => { setFormReg(!formReg) }}>+ Add New</button>
+                <br /><br />
+                <table id="driver-table">
+                    <tr>
+                        <th id='driver-number'>Id</th>
+                        <th>NIC Number</th>
+                        <th>Name</th>
+                        <th>Mobile Number</th>
+                        <th>Email</th>
+                        <th id='driver-action'>Action</th>
 
-
-                    <h3 id='register-form-doctor-name-clusteradmin'>Add New Driver </h3>
-                    <form id='register-form-doctor-form-clusteradmin'  onSubmit={addDriver} >
-                        {message ? <p id="message-form-clusteradmin">{message}   <i class="fa-solid fa-xmark close-button-form" onClick={() => { setMessage("") }}></i></p> : null}
-
-                        <table id='medical-staff-view-table'>
-                            
-                            <tr>
-                                <td>First Name</td>
-                                <td>   <input type="text" id="register-form-doctor-input-clusteradmin" placeholder="Enter First name" onChange={(e) => { setFirstName(e.target.value) }} /> <br />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Last Name</td>
-                                <td>   <input type="text" id="register-form-doctor-input-clusteradmin" placeholder="Enter Last name" onChange={(e) => { setLastName(e.target.value) }} /> <br />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>NIC Number</td>
-                                <td>   <input type="text" id="register-form-doctor-input-clusteradmin" placeholder="Enter NIC Number" onChange={(e) => { setNIC(e.target.value) }} /> <br />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Address</td>
-                                <td>   <input type="text" id="register-form-doctor-input-clusteradmin" placeholder="Enter Address" onChange={(e) => { setAddress(e.target.value) }} /> <br />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Email</td>
-                                <td>   <input type="text" id="register-form-doctor-input-clusteradmin" placeholder="Enter Email" onChange={(e) => { setEmail(e.target.value) }} /> <br />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Mobile Number</td>
-                                <td>   <input type="text" id="register-form-doctor-input-clusteradmin" placeholder="Enter Mobile Number" onChange={(e) => { setMobileNumber(e.target.value) }} /> <br />
-                                </td>
-                            </tr>
-                             
-                        </table> <br />
-                        <button type="sumbit" id="sumbit-save-form" > Register </button> {" "}
-                        <button id="sumbit-cancle-form" onClick={() => { setFormReg(!formReg) }}  > Cancel </button> <br /><br />
-
-                    </form>
+                    </tr>
+                    {list}
+                </table>
+                <div id='driver-pageButton'>
+                    <a className='page-navigation'>{"<< Prev"}  </a>
+                    <a className='page-navigation'>1</a>
+                    <a className='page-navigation'>{"Next >>"}</a>
                 </div>
+            </div>
+            <div id={`${formReg ? 'register-form-doctor-clusteradmin-active' : 'register-form-doctor-clusteradmin'}`}>
+
+
+                <h3 id='register-form-doctor-name-clusteradmin'>Add New Driver </h3>
+                <form id='register-form-doctor-form-clusteradmin' onSubmit={addDriver} >
+                    {message ? <p id="message-form-clusteradmin">{message}   <i class="fa-solid fa-xmark close-button-form" onClick={() => { setMessage("") }}></i></p> : null}
+
+                    <table id='medical-staff-view-table'>
+
+                        <tr>
+                            <td>First Name</td>
+                            <td>   <input type="text" id="register-form-doctor-input-clusteradmin" placeholder="Enter First name" onChange={(e) => { setFirstName(e.target.value) }} /> <br />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Last Name</td>
+                            <td>   <input type="text" id="register-form-doctor-input-clusteradmin" placeholder="Enter Last name" onChange={(e) => { setLastName(e.target.value) }} /> <br />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>NIC Number</td>
+                            <td>   <input type="text" id="register-form-doctor-input-clusteradmin" placeholder="Enter NIC Number" onChange={(e) => { setNIC(e.target.value) }} /> <br />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Address</td>
+                            <td>   <input type="text" id="register-form-doctor-input-clusteradmin" placeholder="Enter Address" onChange={(e) => { setAddress(e.target.value) }} /> <br />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Email</td>
+                            <td>   <input type="text" id="register-form-doctor-input-clusteradmin" placeholder="Enter Email" onChange={(e) => { setEmail(e.target.value) }} /> <br />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Mobile Number</td>
+                            <td>   <input type="text" id="register-form-doctor-input-clusteradmin" placeholder="Enter Mobile Number" onChange={(e) => { setMobileNumber(e.target.value) }} /> <br />
+                            </td>
+                        </tr>
+
+                    </table> <br />
+                    <button type="sumbit" id="sumbit-save-form" > Register </button> {" "}
+                    <button id="sumbit-cancle-form" onClick={() => { setFormReg(!formReg) }}  > Cancel </button> <br /><br />
+
+                </form>
+            </div>
         </div>
     )
 }
