935283def9b33f01bcd3052d5bb472b497a17d7e
Dinusha1234-abcd <62270984+Dinusha1234-abcd@users.noreply.github.com>
2022-09-12 11:44:24 +0530

    add server files

---
+++ server/routes/clusterAdmin/registerDonor/selectDonor.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const selectDonor = require('../../../services/clusterAdmin/registerDonor/selectDonor');
+
+
+router.get('/',  async function (req, res) {
+  
+ 
+ 
+  await selectDonor.getDonor().then(
+    (user) => {
+       
+      const donors = user ;
+      return res.json({
+        donors: donors
+      })
+    }).catch((err) => {
+        console.log(err);
+        return res.json({ message: "unsucess" });
+      });
+
+     
+  
+});
+module.exports = router;
